bdfd2b6ea8af1d2ffe2d2846fa0bedd8a6cd1569
Aidan <survivalking4@gmail.com>
2022-05-01 22:58:32 -0700

    aaa

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function run() {
     var reemoji = emojis.join(String.fromCodePoint(0x200D))
     output.innerText = reemoji;
 }
+
+function insert() {
+    var cp = parseInt(prompt("U+..."), 16);
+    input.value += String.fromCodePoint(cp);
+    run();
+}
