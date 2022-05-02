aa2ff56d8aecd7d23eef34759b63ac7743ba92d8
Aidan <survivalking4@gmail.com>
2022-05-01 23:03:59 -0700

    a

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ function run() {
 
 function insert() {
     var cp = parseInt(prompt("U+..."), 16);
-    input.value += String.fromCodePoint(cp);
+    insertTextAtCursor(input, String.fromCodePoint(cp))
     run();
+}
+
+// https://stackoverflow.com/questions/7404366/how-do-i-insert-some-text-where-the-cursor-is
+function insertTextAtCursor(el, text) {
+    var val = el.value, endIndex, range, doc = el.ownerDocument;
+    if (typeof el.selectionStart == "number"
+            && typeof el.selectionEnd == "number") {
+        endIndex = el.selectionEnd;
+        el.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+        el.selectionStart = el.selectionEnd = endIndex + text.length;
+    } else if (doc.selection != "undefined" && doc.selection.createRange) {
+        el.focus();
+        range = doc.selection.createRange();
+        range.collapse(false);
+        range.text = text;
+        range.select();
+    }
 }
